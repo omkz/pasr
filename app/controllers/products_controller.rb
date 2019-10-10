11b1091b93dac95ec91a16fe7d3632bf3ef26b96
Kurnia Muhamad <omenkzz@gmail.com>
2019-10-11 04:01:46 +0700

    Update listing form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,7 +57,7 @@ class ProductsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def product_params
-    params.require(:product).permit(:name, :price, :description, :image, :user_id)
+    params.require(:product).permit(:name, :price, :description, :image, :product_category_id, :user_id)
   end
 
 end
