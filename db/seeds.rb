7aafcae5631a667fb6c23d8fc624990279f84d32
Kurnia Muhamad <omenkzz@gmail.com>
2019-10-11 05:46:40 +0700

    Update database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
-
 ProductCategory.delete_all
 ProductCategory.create! id: 1, name: "accessories", description: "null"
-ProductCategory.create! id: 1, name: "necklace", description: "null"
-ProductCategory.create! id: 1, name: "rings", description: "null"
-ProductCategory.create! id: 1, name: "earings", description: "null"
+ProductCategory.create! id: 2, name: "necklace", description: "null"
+ProductCategory.create! id: 3, name: "rings", description: "null"
+ProductCategory.create! id: 4, name: "earings", description: "null"
+
+Product.delete_all
+Product.create! id: 1, name: "Mid-Century Modern Solid Walnut ", price: 200, active: true, product_category_id: 1
+Product.create! id: 2, name: "Pallet Sign / Last Name Sign", price: 300, active: true, product_category_id: 2
+Product.create! id: 3, name: "Fireplace Mantel", price: 400, active: true, product_category_id: 3
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
