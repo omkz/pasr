4344185dc041e853108380b26cb023ed5f66378c
Kurnia Muhamad <omenkzz@gmail.com>
2019-10-12 19:57:49 +0700

    Fix listing page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :update, :destroy]
 
   def index
-    @products = Product.includes(image_attachment: :blob).where("vendor_id = ?", current_user.id)
+    @products = Product.includes(image_attachment: :blob).where("vendor_id = ?", current_user.vendor)
   end
 
   def new
