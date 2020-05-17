c7d6de82b581801005a60f3205c27ab2e62d1c43
Kurnia Muhamad <omenkzz@gmail.com>
2020-05-18 01:15:07 +0700

    Update product price validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :image, presence: true
 
