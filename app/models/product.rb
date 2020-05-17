482e612524b57de0fef1d9ab6de5cb6818d7e63a
Kurnia Muhamad <omenkzz@gmail.com>
2020-05-17 21:16:14 +0700

    Update product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,9 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
 
   default_scope { where(active: true) }
 
